62a58733142f6dd4493f611c8c2dc527996db384
sinisaabramovic <sinisha.abramovich@gmail.com>
2020-02-08 15:45:52 +0100

    remove emum type

--- Sources/App/Models/Pet.swift
+++ Sources/App/Models/Pet.swift
@@ -1,19 +1,14 @@
+import Foundation
 import Vapor
 import FluentPostgreSQL
-
-enum PetType: String, PostgreSQLEnum, PostgreSQLMigration, Codable  {
-    case Dog
-    case Cat
-    case Fish
-}
 
 final class Pet: Codable {
     var id: Int?
     var name: String
-    var type: PetType
+    var type: String
     var age: Int
     
-    init(name: String, type: PetType, age: Int) {
+    init(name: String, type: String, age: Int) {
         self.name = name
         self.type = type
         self.age = age
@@ -28,8 +23,8 @@ extension Pet: Content {}
 
 extension Pet: Parameter {}
 
-extension PetType: ReflectionDecodable {
-    static func reflectDecoded() throws -> (PetType, PetType) {
-        return (.Dog, .Cat)
-    }
-}
+// Useful Docker commands
+//docker exec -it postgres psql -U vapor -d vapor
+//docker exec -it postgres psql -U vapor -d postgres -c "DROP DATABASE vapor;"
+//docker exec -it postgres psql -U vapor -d postgres -c "CREATE DATABASE vapor;"
+
